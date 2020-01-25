6521200d70f395ee3d789748ba767cb12339d65c
Matthew Oberg <matthewroberg@gmail.com>
2020-01-25 13:46:22 -0800

    Initial Commit

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,31 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+
+public class Robot extends TimedRobot {
+
+  @Override
+  public void robotInit() {
+
+  }
+
+  @Override
+  public void autonomousInit() {
+
+  }
+
+  @Override
+  public void autonomousPeriodic() {
+
+  }
+
+  @Override
+  public void teleopInit() {
+
+  }
+
+  @Override
+  public void teleopPeriodic() {
+
+  }
+}
